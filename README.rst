52bfe2ee0eae66bbf53c06064cc118dd200dff6f
estrellafg <estrellafergi@gmail.com>
2020-01-21 11:14:14 +0100

    restructuring

---
+++ README.rst
@@ -0,0 +1,18 @@
+=======================
+Scipion template plugin
+=======================
+
+This is a template plugin for **scipion**
+
+=====
+Setup
+=====
+
+- **Install this plugin in devel mode:**
+
+Using the command line:
+
+.. code-block::
+
+    scipion installp -p local/path/to/scipion-em-template --devel
+
---
+++ xmipptomo/wizards.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# *
+# * Authors:     Estrella Fernandez Gimenez (me.fernandez@cnb.csic.es)
+# *
+# * [1] Unidad de Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+from pyworkflow.utils import importFromPlugin
+from pyworkflow.wizard import Wizard
+
+XmippProtConnectedComponents = importFromPlugin("xmipptomo.protocols",
+                                                "XmippProtConnectedComponents")
+
+class XmippConnectedCompWizard(Wizard):
+    _targets = ([(XmippProtConnectedComponents, ['distance'])]
+                if XmippProtConnectedComponents is not None else [])
+
+    def show(self, form):
+        tomoCCProt = form.protocol
+        inputCoordinates = tomoCCProt.inputCoordinates.get()
+        if not inputCoordinates:
+            print('You must specify input coordinates')
+            return
+        boxSize = inputCoordinates.getBoxSize()
+        if not boxSize:
+            print('These coordinates do not have box size. Please, enter distance manually.')
+            return
+        distance = boxSize * 3
+        form.setVar('distance', distance)
