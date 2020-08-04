02fa2d3d027554291e0e3afb994e732fc1fe1e4a
David Strelak <davidstrelak@gmail.com>
2020-08-04 13:12:11 +0200

    adding FlexAlign for tilt series alignment

---
+++ xmipptomo/protocols/protocol_flexalign.py
@@ -0,0 +1,53 @@
+# **************************************************************************
+# *
+# * Authors:     David Strelak (davidstrelak@gmail.com)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+from tomo.protocols import ProtTsCorrectMotion
+from xmipp3.protocols import XmippProtMovieCorr
+from pathlib import Path
+
+class XmippProtTsFlexAlign(ProtTsCorrectMotion, XmippProtMovieCorr):
+    """
+    Simple protocol to average TiltSeries movies as basic
+    motion correction. It is used mainly for testing purposes.
+    """
+    _label = 'tiltseries FlexAlign'
+    
+    
+    def _defineParams(self, form):
+        ProtTsCorrectMotion._defineParams(self, form)
+        form.addSection(label="FlexAlign")
+        XmippProtMovieCorr._defineAlignmentParams(self, form)
+
+    def _getOutputMicName(self, movie):
+        return self._getTiltImageMRoot(movie) + ".mrc"
+
+    def _processTiltImageM(self, workingFolder, tiltImageM, *args):
+        # XXX hack - create needed attributes and directories, so that we don't have to change the implementation of the FlexAlign
+        dir = self._getOutputMovieFolder(tiltImageM)
+        Path(dir).mkdir(parents=True, exist_ok=True)
+        self.inputMovies = self.inputTiltSeriesM
+        self._processMovie(tiltImageM)
+        self.inputMovies = None
