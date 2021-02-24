05b29daf41746ee31cbf40e97b5cb8dcfa6e49e7
fede-pe <fepe.ig@gmail.com>
2021-02-24 15:47:24 +0100

    add utils file with method to retrieve coordinates 3d from xmipp to scipion format

---
+++ utils.py
@@ -0,0 +1,43 @@
+# **************************************************************************
+# *
+# * Authors:     Federico P. de Isidro Gomez (fp.deisidro@cnb.csic.es) [1]
+# *
+# * [1] Centro Nacional de Biotecnologia, CSIC, Spain
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+
+def retrieveXmipp3dCoordinatesIntoList(coordFilePath, xDim, yDim, zDim):
+    """ This method takes an xmipp metadata (xmd) 3D coordinates file path and returns a list of tuples containing
+    every coordinate. This method also transform the coordinates into the Scipion convention. """
+
+    coorList = []
+
+    with open(coordFilePath) as f:
+        inputLines = f.readlines()
+
+    for line in inputLines[7:]:
+        vector = line.split()
+        coorList.append([float(vector[1]) - xDim / 2,
+                         float(vector[2]) - yDim / 2,
+                         float(vector[3]) - zDim / 2])
+
+    return coorList
