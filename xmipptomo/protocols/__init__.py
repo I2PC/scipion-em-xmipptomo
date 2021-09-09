17f5db56ee9fa72ca25b4186e9fadca6427ba29b
DavidHerreros <david.herreroscalero@gmail.com>
2021-09-09 08:24:59 +0200

    New protocol to produce two half maps from an aligned SetOfSubTomograms

--- xmipptomo/protocols/__init__.py
+++ xmipptomo/protocols/__init__.py
@@ -46,6 +46,7 @@ from .protocol_score_transform import XmippProtScoreTransform
 from .protocol_splitTS import XmippProtSplitTiltSeries
 from .protocol_subtraction_subtomo import XmippProtSubtractionSubtomo
 from .protocol_subtomo_map_back import XmippProtSubtomoMapBack
+from .protocol_half_maps_subtomos import XmippProtHalfMapsSubtomo
 
 
 
