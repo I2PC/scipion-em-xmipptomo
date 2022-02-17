ba8a0c5db701eb269806cecc82f5987ce458a9e1
fede-pe <fepe.ig@gmail.com>
2022-02-17 14:06:33 +0100

    add porotol to init

--- xmipptomo/protocols/__init__.py
+++ xmipptomo/protocols/__init__.py
@@ -32,6 +32,7 @@ from .protocol_applyAlignmentTS import XmippProtApplyTransformationMatrixTS
 from .protocol_cltomo import XmippProtCLTomo
 from .protocol_connected_components import XmippProtConnectedComponents
 from .protocol_coords_roi import XmippProtCCroi
+from .protocol_filter_coordinates_by_map import XmippProtFilterCoordinatesByMap
 from .protocol_fit_ellipsoid import XmippProtFitEllipsoid
 from .protocol_flexalign import XmippProtTsFlexAlign
 from .protocol_misalignTS import XmippProtMisalignTiltSeries
