ca49793e81a4ed75cf254b4dd67f5bc8b1433cff
fede-pe <fepe.ig@gmail.com>
2022-05-19 17:55:00 +0200

    fixes in converti input step interpolation

--- xmipptomo/protocols/protocol_detect_misalignment_TS.py
+++ xmipptomo/protocols/protocol_detect_misalignment_TS.py
@@ -143,11 +143,16 @@ class XmippProtDetectMisalignmentTiltSeries(EMProtocol, ProtTomoBase):
 
         """Apply the transformation form the input tilt-series"""
         # Use Xmipp interpolation via Scipion
-        avgRotAngle = utils.calculateRotationAngleFromTM(ts)
-        swap = True if (avgRotAngle > 45 or avgRotAngle < -45) else False
+        if firstItem.hasTransform():
+            avgRotAngle = utils.calculateRotationAngleFromTM(ts)
+            swap = True if (avgRotAngle > 45 or avgRotAngle < -45) else False
 
-        outputTsFileName = os.path.join(tmpPrefix, firstItem.parseFileName())
-        ts.applyTransform(outputTsFileName, swapXY=swap)
+            outputTsFileName = os.path.join(tmpPrefix, firstItem.parseFileName())
+            ts.applyTransform(outputTsFileName, swapXY=swap)
+
+        else:
+            outputTsFileName = os.path.join(tmpPrefix, firstItem.parseFileName())
+            ts.applyTransform(outputTsFileName)
 
         """Generate angle file"""
         angleFilePath = os.path.join(tmpPrefix, firstItem.parseFileName(extension=".tlt"))
