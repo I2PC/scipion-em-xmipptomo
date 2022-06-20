5c069612d316cd30f41198dd2494b12fcdd325c6
fede-pe <fepe.ig@gmail.com>
2022-06-20 18:21:01 +0200

    end of session

--- xmipptomo/protocols/protocol_ts_calculate_3d_coordinate.py
+++ xmipptomo/protocols/protocol_ts_calculate_3d_coordinate.py
@@ -40,7 +40,7 @@ from tomo.protocols import ProtTomoBase
 import xmipptomo.utils as utils
 
 
-class XmippProtAverageViewTiltSeries(EMProtocol, ProtTomoBase):
+class XmippProtCalculate3dCoordinatesFromTS(EMProtocol, ProtTomoBase):
     """
     Scipion protocol to calculate the 3D coordinate based on a set of 2D coordinates obtained from the tilt-series
     using a SPA 2D picker.
@@ -59,13 +59,62 @@ class XmippProtAverageViewTiltSeries(EMProtocol, ProtTomoBase):
 
         form.addParam('inputSetOfCoordinates',
                       PointerParam,
-                      pointerClass='SetOfCoordinate',
+                      pointerClass='SetOfCoordinates',
                       important=True,
-                      label='Input set of 2D coordinates')
+                      label='Input set of 2D coordinates form tilt-series')
 
     # -------------------------- INSERT steps functions ---------------------
     def _insertAllSteps(self):
-
+        self._insertFunctionStep(self.convertInputStep)
 
     # --------------------------- STEPS functions ----------------------------
 
+    def convertInputStep(self):
+        print(self.inputSetOfCoordinates.get().getMicrographs())
+        for c in self.inputSetOfCoordinates.get():
+            print(c)
+            print(c.getMicId())
+        # """ Read the input metadatata. """
+        # # Get the converted input micrographs in Xmipp format
+        # makePath(self._getExtraPath("untilted"))
+        # makePath(self._getExtraPath("tilted"))
+        #
+        # uSet = self.untiltedSet.get()
+        # tSet = self.tiltedSet.get()
+        #
+        # # Get the untilted and tilted coordinates, depending on the input type
+        # if isinstance(uSet, SetOfParticles):
+        #     uCoords = uSet.getCoordinates()
+        #     tCoords = tSet.getCoordinates()
+        #
+        #     # If there are not Coordinates associated to particles
+        #     # we need to create and fill the set of coordinates
+        #     if uCoords is None or tCoords is None:
+        #         micTiltedPairs = self.inputMicrographsTiltedPair.get()
+        #         uCoords = self._coordsFromParts(micTiltedPairs.getUntilted(),
+        #                                         uSet, '_untilted')
+        #         tCoords = self._coordsFromParts(micTiltedPairs.getTilted(),
+        #                                         tSet, '_tilted')
+        # else:
+        #     uCoords = uSet
+        #     tCoords = tSet
+        #
+        # writeSetOfCoordinates(self._getExtraPath("untilted"), uCoords)
+        # writeSetOfCoordinates(self._getExtraPath("tilted"), tCoords)
+
+
+    def assignmentStep(self, fnuntilt, fntilt, fnmicsize, fnposUntilt, fnposTilt):
+        params = ' --untiltcoor %s' % fnuntilt
+        params += ' --tiltcoor %s' % fntilt
+        params += ' --tiltmicsize %s' % fnmicsize
+        params += ' --maxshift %f' % self.maxShift
+        params += ' --particlesize %d' % self._getBoxSize()
+        params += ' --threshold %f' % self.threshold
+        params += ' --odir %s' % self._getExtraPath()
+        self.runJob('xmipp_image_assignment_tilt_pair', params)
+
+        # Estimate the tilt axis
+        params = ' --untilted %s' % fnposUntilt
+        params += ' --tilted %s' % fnposTilt
+        params += ' -o %s' % self._getPath('input_micrographs.xmd')
+        self.runJob('xmipp_angular_estimate_tilt_axis', params)
