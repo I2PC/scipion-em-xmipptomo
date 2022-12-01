fix normalization

xmipptomo/protocols/protocol_deep_misalignment_detection.py
@@ -120,15 +120,15 @@ class XmippProtDeepDetectMisalignment(EMProtocol, ProtTomoBase):
 
         subtomoArray = np.zeros((numberOfSubtomos, 32, 32, 32), dtype=np.float64)
 
-        std = subtomoArray.std()
-        mean = subtomoArray.mean()
-
-        subtomoArray = (subtomoArray - mean) / std
-
         for index, subtomoFilePath in enumerate(subtomoFilePathList):
             subtomoDataTmp = ih.read(subtomoFilePath).getData()
 
             subtomoArray[index, :, :, :] = subtomoDataTmp[:, :, :]
+
+        std = subtomoArray.std()
+        mean = subtomoArray.mean()
+
+        subtomoArray = (subtomoArray - mean) / std
 
         predictionArray = self.firstModel.predict(subtomoArray)
 