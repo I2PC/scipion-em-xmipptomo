fae9030229abaebc9b08abdc37feef82f8553532
fede-pe <fepe.ig@gmail.com>
2023-01-30 17:20:59 +0100

    test passing

--- xmipptomo/tests/test_protocols_xmipptomo.py
+++ xmipptomo/tests/test_protocols_xmipptomo.py
@@ -23,6 +23,7 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
+import os
 
 import numpy as np
 from pwem.emlib.image import ImageHandler
@@ -30,11 +31,12 @@ from pyworkflow.tests import BaseTest, setupTestProject
 from tomo.tests import DataSet
 from tomo.protocols import (ProtImportCoordinates3D,
                             ProtImportTomograms,
-                            ProtImportSubTomograms)
+                            ProtImportSubTomograms,
+                            ProtImportTs)
 
 from xmipptomo.protocols import XmippProtSubtomoProject, XmippProtConnectedComponents, XmippProtApplyTransformSubtomo, \
     XmippProtSubtomoMapBack, XmippProtPhantomSubtomo, XmippProtScoreCoordinates, XmippProtScoreTransform, \
-    XmippProtHalfMapsSubtomo, XmippProtPhantomTomo, XmippProtSubtractionSubtomo
+    XmippProtHalfMapsSubtomo, XmippProtPhantomTomo, XmippProtSubtractionSubtomo, XmippProtSplitTiltSeries
 from xmipptomo.protocols.protocol_phantom_tomo import OutputPhantomTomos
 from xmipptomo.protocols.protocol_project_top import SubtomoProjectOutput
 
@@ -436,5 +438,62 @@ class TestXmipptomoSubtractionSubtomo(BaseTest):
         self.assertSetSize(subtraction.outputSubtomograms, 10, "There was a problem with particles output")
         self.assertTrue(subtraction.outputSubtomograms.getSamplingRate() == 1.0)
         self.assertTrue(subtraction.outputSubtomograms.getFirstItem().getDim() == (40, 40, 40))
+
+
+class TestXmipptomoSplitTS(BaseTest):
+    """This class check if the protocol split tilt-series works properly."""
+
+    @classmethod
+    def setUpClass(cls):
+        setupTestProject(cls)
+
+        cls.inputDataSet = DataSet.getDataSet('tomo-em')
+        cls.inputSoTS = cls.inputDataSet.getFile('ts1_imod')
+
+    def runWorkflow(self):
+        protImportTS = self.newProtocol(ProtImportTs,
+                                           filesPath=os.path.split(self.inputSoTS)[0],
+                                           filesPattern="BB{TS}.st",
+                                           voltage=300,
+                                           anglesFrom=0,
+                                           magnification=105000,
+                                           sphericalAberration=2.7,
+                                           amplitudeContrast=0.1,
+                                           samplingRate=20.2,
+                                           doseInitial=0,
+                                           dosePerFrame=3.0,
+                                           minAngle=-55,
+                                           maxAngle=65.0,
+                                           stepAngle=2.0,
+                                           tiltAxisAngle=-12.5)
+
+        self.launchProtocol(protImportTS)
+
+        protSplitTS = self.newProtocol(XmippProtSplitTiltSeries,
+                                         inputSetOfTiltSeries=protImportTS.outputTiltSeries)
+
+        self.launchProtocol(protSplitTS)
+
+        return protSplitTS
+
+    def testSplitTS(self):
+        protSplitTS = self.runWorkflow()
+
+        self.assertIsNotNone(protSplitTS.outputEvenSetOfTiltSeries,
+                             "Even set of tilt series has not been generated")
+        self.assertIsNotNone(protSplitTS.outputOddSetOfTiltSeries,
+                             "Odd set of tilt series has not been generated")
+
+        self.assertTrue(protSplitTS.outputEvenSetOfTiltSeries.getSamplingRate() == 20.20)
+        self.assertTrue(protSplitTS.outputOddSetOfTiltSeries.getSamplingRate() == 20.20)
+
+        self.assertSetSize(protSplitTS.outputEvenSetOfTiltSeries, 2, "Missing TS in even set")
+        self.assertSetSize(protSplitTS.outputOddSetOfTiltSeries, 2, "Missing TS in odd set")
+
+        print(protSplitTS.outputEvenSetOfTiltSeries.getFirstItem().getDim())
+        print(protSplitTS.outputOddSetOfTiltSeries.getFirstItem().getDim())
+        self.assertTrue(protSplitTS.outputEvenSetOfTiltSeries.getFirstItem().getDim() == (512, 512, 30))
+        self.assertTrue(protSplitTS.outputOddSetOfTiltSeries.getFirstItem().getDim() == (512, 512, 31))
+
 
 
