da8cdf59f9823337a9d0b945227e50c924ea9b8e
JorMaister <jorgejjs86@gmail.com>
2023-10-30 16:50:05 +0100

    Improve coordinates matching

--- xmipptomo/protocols/protocol_score_transform.py
+++ xmipptomo/protocols/protocol_score_transform.py
@@ -27,10 +27,12 @@ import enum
 import math
 
 import numpy as np
+from scipy.spatial.distance import cdist
 
 from pyworkflow import BETA
 from pyworkflow.object import Float
 from pyworkflow.protocol import params
+from tomo.constants import SCIPION
 
 from tomo.protocols import ProtTomoPicking
 from tomo.objects import SetOfSubTomograms
@@ -40,6 +42,7 @@ from pwem.convert.transformations import listQuaternions, quaternion_distance
 
 class ScoreTransformOutputs(enum.Enum):
     Subtomograms = SetOfSubTomograms
+
 
 class XmippProtScoreTransform(ProtTomoPicking):
     """Protocol to score a series of alignments stored in a SetOfSubtomograms by
@@ -57,6 +60,11 @@ class XmippProtScoreTransform(ProtTomoPicking):
     def __init__(self, **args):
         ProtTomoPicking.__init__(self, **args)
         # self.stepsExecutionMode = STEPS_PARALLEL
+        self._percentage_outliers = None
+        self.scoresIndex = None
+        self._mean_dist = None
+        self.first_subtomos = None
+        self.second_subtomos = None
 
     def _defineParams(self, form):
         form.addSection(label='Input')
@@ -72,15 +80,13 @@ class XmippProtScoreTransform(ProtTomoPicking):
         # For convenience
         self.first_subtomos = self.firstSubtomos.get()
         self.second_subtomos = self.secondSubtomos.get()
-
         self._insertFunctionStep(self.scoreTransformStep)
 
     # --------------------------- STEPS functions ---------------------------
     def scoreTransformStep(self):
-
         # Extract Transformation Matrices from input SubTomograms
-        first_matrices = self.queryMatrices(self.first_subtomos)
-        second_matrices = self.queryMatrices(self.second_subtomos)
+        # first_matrices, second_matrices = self.getMatricesFromCommonItems()
+        first_matrices, second_matrices = self.getTransformMatrices()
 
         # Convert Trasnformation Matrices to Quaternions
         aux = list(zip(*first_matrices))
@@ -108,12 +114,10 @@ class XmippProtScoreTransform(ProtTomoPicking):
         self._percentage_outliers = Float(percentage_outliers)
         self._store()
 
-
     def createOutput(self, distanceScores):
 
-        outSubtomos = self.firstSubtomos.get().create(self._getPath())
+        outSubtomos = SetOfSubTomograms.create(self._getPath(), template='submograms%s.sqlite')
         outSubtomos.copyInfo(self.second_subtomos)
-
         self.scoresIndex = 0
 
         def addScoreToSubtomogram(subtomo, row):
@@ -133,15 +137,69 @@ class XmippProtScoreTransform(ProtTomoPicking):
 
         outSubtomos.copyItems(self.first_subtomos, updateItemCallback=addScoreToSubtomogram)
 
-        self._defineOutputs(**{ScoreTransformOutputs.Subtomograms.name:outSubtomos})
+        self._defineOutputs(**{ScoreTransformOutputs.Subtomograms.name: outSubtomos})
         self._defineSourceRelation(self.firstSubtomos, outSubtomos)
         self._defineSourceRelation(self.secondSubtomos, outSubtomos)
 
     # --------------------------- UTILS functions ---------------------------
-    def queryMatrices(self, subtomos):
-        matrices = [(subtomo.getObjId(), subtomo.getTransform().getMatrix())
-                    for subtomo in subtomos.iterItems()]
-        return matrices
+    # def queryMatrices(self, subtomos):
+    #     matrices = [(subtomo.getObjId(), subtomo.getTransform().getMatrix())
+    #                 for subtomo in subtomos.iterItems()]
+    #     return matrices
+
+    def getMatricesFromCommonItems(self):
+        objIds1 = [str(item.getObjId()) for item in self.first_subtomos]  # Id list present in the first set
+        objIds2 = [str(item.getObjId()) for item in self.second_subtomos]  # The same for the second set
+        commonInds = set(objIds1) & set(objIds2)  # Get the Ids present in the intersection of both lists
+        whereCond = f'id IN ({",".join(commonInds)})'
+        # Get the transformation lists from both sets considering only the common ids and iterated following the same
+        # order
+        tr1 = [item.getTransform().getMatrix() for item in self.first_subtomos._getMapper().selectAll(where=whereCond)]
+        tr2 = [item.getTransform().getMatrix() for item in self.second_subtomos._getMapper().selectAll(where=whereCond)]
+        return tr1, tr2
+
+    def getMatchingCoordsFromSubtomos(self, minDistance=1):
+        coordDictList1 = {part.clone(): coord.getPosition(SCIPION) for part, coord in zip(self.first_subtomos, self.first_subtomos.getCoordinates3D())}
+        coordDictList2 = {part.clone(): coord.getPosition(SCIPION) for part, coord in zip(self.second_subtomos, self.second_subtomos.getCoordinates3D())}
+        coordList1 = np.array(list(coordDictList1.values()))
+        coordList2 = np.array(list(coordDictList2.values()))
+        numel1 = len(coordList1)
+        numel2 = len(coordList2)
+        if numel1 == numel2 or numel1 < numel2:
+
+            distances = cdist(coordList1, coordList2)
+        else:
+            distances = cdist(coordList2, coordList1)
+
+        indices = np.argmin(distances, axis=1)  # Indices of min distance
+        # m x 2 array, being the min distance indices the first column and the min distance value the second
+        minDistances = np.min(distances, axis=1)
+        indsAndDistances1 = np.column_stack((range(len(indices)), minDistances))
+        indsAndDistances2 = np.column_stack((indices, minDistances))
+
+        # Filter elements using the min distance value provided
+        finalIndices1 = np.where(indsAndDistances1[:, 1] < minDistance)
+        finalIndices2 = np.where(indsAndDistances2[:, 1] < minDistance)
+
+        # Generate the final lists of coordinates objects
+        subtomos1 = list(coordDictList1.keys())
+        subtomos2 = list(coordDictList2.keys())
+        # [0] in the comprehensions below is because the previous calls to np.where() returns a 1-element tuple
+        # containing the desired list of indices
+        finalList1 = [subtomos1[ind] for ind in finalIndices1[0]]
+        finalList2 = [subtomos2[ind] for ind in finalIndices2[0]]
+        return finalList1, finalList2
+
+    def getTransformMatrices(self):
+        subtomos1, subtomos2 = self.getMatchingCoordsFromSubtomos()
+        transforms1 = []
+        transforms2 = []
+        for part1, part2 in zip(subtomos1, subtomos2):
+            transforms1.append(part1.getTransform().getMatrix())
+            transforms2.append(part2.getTransform().getMatrix())
+
+        return transforms1, transforms2
+
 
     # --------------------------- INFO functions ---------------------------
     def _summary(self):
