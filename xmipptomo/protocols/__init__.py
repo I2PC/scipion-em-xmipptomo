0a88b741ae45e0691f36203874527fe3e0bb0d37
Vilax <jlvilasprieto@gmail.com>
2024-01-25 15:38:08 +0100

    removing code

--- xmipptomo/protocols/__init__.py
+++ xmipptomo/protocols/__init__.py
@@ -40,7 +40,6 @@ from .protocol_extract_subtomos import XmippProtExtractSubtomos
 from .protocol_flexalign import XmippProtTsFlexAlign
 from .protocol_peak_high_contrast import XmippProtPeakHighContrast
 from .protocol_phantom_subtomo import XmippProtPhantomSubtomo
-from .protocol_phantom_tilt_series import XmippProtPhantomTiltSeries
 from .protocol_phantom_tomo import XmippProtPhantomTomo
 #from .protocol_project_top import XmippProtSubtomoProject
 from .protocol_reconstruct_initialVol import XmippProtReconstructInitVol
